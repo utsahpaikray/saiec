b1e995e7c5b6c5c6c277f97e4554276d475e87bb
utsahpaikray <utsahpaikray@gmail.com>
2022-05-07 07:24:22 +0530

    ui changes

--- src/app/modules/questionset/questionset.page.ts
+++ src/app/modules/questionset/questionset.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questionset.page.scss'],
 })
 export class QuestionsetPage implements OnInit {
-
+public questionSet=[]
   constructor() { }
 
   ngOnInit() {
+    this.questionSet=[
+      {
+        question:'Which of the following is not a Union Territory',
+        options:[
+         'Ladakh',
+         'Chandigarh',
+         'Jammu and Kashmir',
+        ' Sikkim'
+        ],
+        answer:'Jammu and Kashmir',
+      }
+    ]
   }
 
 }
