25cf015ed321cda5c4efd709b7b0716ecb4a5047
utsahpaikray <utsahpaikray@gmail.com>
2022-05-14 17:26:20 +0530

    Search functionality

--- src/app/modules/student/student.page.ts
+++ src/app/modules/student/student.page.ts
@@ -5,6 +5,7 @@ import { std4 } from './../../../assets/student-info/standard4';
 import { std3 } from './../../../assets/student-info/standard3';
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx'
+import { groupBy,values } from 'lodash';
 @Component({
   selector: 'app-student',
   templateUrl: './student.page.html',
@@ -16,16 +17,41 @@ export class StudentPage implements OnInit {
   public student5Info;
   public student4Info;
   public student3Info;
+  public gridValue:boolean=false;
+  searchQuery: any;
+  allStudent: any[];
+  itemsCount: number=1;
+  allStudentClassWise: any[];
   constructor() { 
      this.student7Info=std7;
      this.student6Info=std6;
      this.student5Info=std5;
      this.student4Info=std4;
      this.student3Info=std3;
+     this.allStudent=[...this.student7Info,...this.student6Info,...this.student5Info,...this.student4Info,...this.student3Info]
+    
+     
   }
 
   ngOnInit() {
-    console.log(std7)
+    this.allStudentClassWise= values(groupBy(this.allStudent, 'Class'))
+  }
+  public onInput(){
+    const items = Array.from(document.querySelector('.student-info').children);
+     this.itemsCount=0;
+    requestAnimationFrame(() => {
+      items.forEach((item) => {
+        Array.from(item.children).forEach((item) => {
+       const shouldShow = item.textContent.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1;
+       this.itemsCount=shouldShow?this.itemsCount+1:this.itemsCount;
+       item['style'].display = shouldShow ? 'block' : 'none';
+        })
+      }); 
+
+    });
+  }
+  public ongrid(){
+  
   }
   // public selectedFile(event){
   //  let  selectedFile = event.target.files[0];
