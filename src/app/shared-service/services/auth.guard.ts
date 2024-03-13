ae1a176eed84e719b03131c6c9c16b108dcca921
Utsah.Paikray <utsah.paikray@vanderlande.com>
2024-03-13 07:52:39 +0530

    16

--- src/app/shared-service/services/auth.guard.ts
+++ src/app/shared-service/services/auth.guard.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../auth-service.service';
+import { inject } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
-  constructor(
-    private authService: AuthService,
-    private router: Router) { }
+export class AuthGuard  {
+
+    public router = inject(Router)
+    private authService= inject(AuthService)
     canActivate(
       route: ActivatedRouteSnapshot,
       state: RouterStateSnapshot): boolean | Promise<boolean> {
